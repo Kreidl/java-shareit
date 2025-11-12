c288c1e8a293460752fc104da3c626d08f1275d6
Карина <karinkademchenko@yandex.ru>
2025-11-12 14:07:56 +0300

    Добавлены интерфейсы сервисов

---
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -0,0 +1,21 @@
+package ru.practicum.shareit.item.service;
+
+import ru.practicum.shareit.item.model.dto.ItemCreateDto;
+import ru.practicum.shareit.item.model.dto.ItemDto;
+import ru.practicum.shareit.item.model.dto.ItemUpdateDto;
+
+import java.util.Collection;
+
+public interface ItemService {
+    ItemDto createItem(ItemCreateDto itemCreateDto, long ownerId);
+
+    ItemDto updateItem(ItemUpdateDto itemUpdateDto, long itemId, long ownerId);
+
+    ItemDto getItemById(long itemId);
+
+    Collection<ItemDto> getUserItems(long ownerId);
+
+    Collection<ItemDto> getSearchItems(String text);
+
+    void deleteItemById(long itemId, long ownerId);
+}
---
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -0,0 +1,15 @@
+package ru.practicum.shareit.user.service;
+
+import ru.practicum.shareit.user.model.dto.UserCreateDto;
+import ru.practicum.shareit.user.model.dto.UserDto;
+import ru.practicum.shareit.user.model.dto.UserUpdateDto;
+
+import java.util.Collection;
+
+public interface UserService {
+    UserDto createUser(UserCreateDto user);
+    UserDto updateUser(UserUpdateDto updatedUser, long userId);
+    Collection<UserDto> getAllUsers();
+    UserDto getUserById(long userId);
+    void deleteUserById(long userId);
+}
