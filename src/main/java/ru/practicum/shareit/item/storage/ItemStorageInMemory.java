34401227c1181a66847ee1e9f1dc04340fcbdb29
Карина <karinkademchenko@yandex.ru>
2025-11-12 14:10:06 +0300

    Добавлены сервис и репозиторий для предметов

---
+++ src/main/java/ru/practicum/shareit/item/storage/ItemStorageInMemory.java
@@ -0,0 +1,61 @@
+package ru.practicum.shareit.item.storage;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+@Repository
+@Slf4j
+@Data
+public class ItemStorageInMemory implements ItemStorage {
+    private long itemCount = 1;
+    private final HashMap<Long, Item> items;
+
+    @Override
+    public Item createItem(Item item) {
+        item.setId(itemCount);
+        itemCount++;
+        items.put(item.getId(), item);
+        return item;
+    }
+
+    @Override
+    public Item updateItem(Item updatedItem) {
+        Item item = items.get(updatedItem.getId());
+        item.setName(updatedItem.getName());
+        item.setDescription(updatedItem.getDescription());
+        item.setAvailable(updatedItem.isAvailable());
+        return item;
+    }
+
+    @Override
+    public Item getItemById(long itemId) {
+        return items.get(itemId);
+    }
+
+    @Override
+    public Collection<Item> getUserItems(long ownerId) {
+        return items.values().stream()
+                .filter(item -> item.getOwner().getId() == ownerId)
+                .toList();
+    }
+
+    @Override
+    public Collection<Item> getSearchItems(String text) {
+        String finalText = text.toLowerCase();;
+        return items.values().stream()
+                .filter(item -> item.getName().toLowerCase().contains(finalText) ||
+                        item.getDescription().toLowerCase().contains(finalText))
+                .filter(Item::isAvailable)
+                .toList();
+    }
+
+    @Override
+    public void deleteItemById(long itemId) {
+        items.remove(itemId);
+    }
+}
