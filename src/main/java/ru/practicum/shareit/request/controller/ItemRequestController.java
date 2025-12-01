c367b11a9bbd3c9314e112c34eaee7b1a981e000
Карина <karinkademchenko@yandex.ru>
2025-12-02 01:50:00 +0300

    Изменение ItemRequest

--- src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
@@ -1,14 +1,14 @@
 package ru.practicum.shareit.request.controller;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-@RequiredArgsConstructor
-@RestController
-@Slf4j
-@Validated
-@RequestMapping(path = "/requests")
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RequiredArgsConstructor
+//@RestController
+//@Slf4j
+//@Validated
+//@RequestMapping(path = "/requests")
 public class ItemRequestController {
 }
--- src/main/java/ru/practicum/shareit/request/model/mapper/ItemRequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/model/mapper/ItemRequestMapper.java
@@ -1,12 +1,12 @@
 package ru.practicum.shareit.request.model.mapper;
 
-import ru.practicum.shareit.request.model.ItemRequest;
-import ru.practicum.shareit.request.model.dto.ItemRequestDto;
-
+//import ru.practicum.shareit.request.model.ItemRequest;
+//import ru.practicum.shareit.request.model.dto.ItemRequestDto;
+//
 public class ItemRequestMapper {
-    public ItemRequestDto toItemRequestDto(ItemRequest itemRequest) {
-        return new ItemRequestDto(itemRequest.getDescription(),
-                itemRequest.getRequester(),
-                itemRequest.getCreated());
-    }
+//    public ItemRequestDto toItemRequestDto(ItemRequest itemRequest) {
+//        return new ItemRequestDto(itemRequest.getDescription(),
+//                itemRequest.getRequester(),
+//                itemRequest.getCreated());
+//    }
 }
